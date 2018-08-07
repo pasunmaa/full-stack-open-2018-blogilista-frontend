88aa8310f693861d605b987438e4da2e91d1eae3
Asunmaa <petri.asunmaa@accenture.com>
2018-08-07 09:31:09 +0300

    Tehtävä 5.12 valmis

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -3,19 +3,30 @@ import { shallow } from 'enzyme'
 import SimpleBlog from './SimpleBlog'
 
 describe.only('<SimpleBlog />', () => {
-  it('renders title, author and likes', () => {
-    const blog = {
-      title: 'React patterns',
-      author: 'Michael Chan',
-      url: 'https://reactpatterns.com/',
-      likes: 7,
-    }
+  const blog = {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  }
 
+  it('renders title, author and likes', () => {
     const blogComponent = shallow(<SimpleBlog blog={blog} />)
 
     //console.log(blogComponent.debug())
     expect(blogComponent.text()).toContain(blog.title)
     expect(blogComponent.text()).toContain(blog.author)
     expect(blogComponent.text()).toContain(blog.likes.toString())
+  })
+
+  it('calls likehandler twice, when button is pressed twice', () => {
+    const mockHandler = jest.fn()  // mock function for likehandler
+    const blogComponent = shallow(<SimpleBlog blog={blog} onClick={mockHandler} />)
+
+    const button = blogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(2)
   })
 })
