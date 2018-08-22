78b4c966d68ac7c986abd59d503704859172fcfe
Asunmaa <petri.asunmaa@accenture.com>
2018-08-22 10:12:46 +0300

    Trying to fix testing: Network error

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,6 +2,7 @@ import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 jest.mock('./services/blogs')
 
+console.log('Running enzyme setup')
 configure({ adapter: new Adapter() })
 
 let savedItems = {}
