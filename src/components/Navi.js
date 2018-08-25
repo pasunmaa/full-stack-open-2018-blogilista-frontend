6d38d1453e2aa5694f5d96d96f928623cd6e36c2
Petri Asunmaa <petrieas@gmail.com>
2018-08-25 11:14:51 +0300

    work-in-progress AppBar

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
 
-const Navi = ( props ) => {
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 10,
+  },
+}
+
+const Navi = ( { name, logout, classes } ) => {
   const menuStyle = {
     backgroundColor: 'lightgreen',
     padding: 10
@@ -14,19 +34,28 @@ const Navi = ( props ) => {
   }
 
   return (
-    <span className='navi' style={menuStyle}>
-      <NavLink exact activeStyle={activeStyle} to="/">blogs</NavLink> &nbsp;
-      <NavLink exact activeStyle={activeStyle} to="/users">users</NavLink> &nbsp;
-      &nbsp; &nbsp;
-      <b>{props.name}</b> kirjautuneena. &nbsp;
-      <button type="button" onClick={props.logout}>kirjaudu ulos</button>
-    </span>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <NavLink exact activeStyle={activeStyle} to="/">blogs</NavLink> &nbsp;
+          <NavLink exact activeStyle={activeStyle} to="/users">users</NavLink> &nbsp; &nbsp; &nbsp;
+          <Typography variant="title" color="inherit" className={classes.flex}>
+            {name} kirjautuneena.
+          </Typography>
+          <Button color="inherit" onClick={logout}>kirjaudu ulos</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
   )
 }
 
 Navi.propTypes = {
   name: PropTypes.string.isRequired,
   logout: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
 }
 
-export default Navi
+export default withStyles(styles)(Navi)
